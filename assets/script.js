fda383d54dbbe00fcc52101f4fbfbed8315bd9b7
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-10-08 16:36:35 -0700

    set up script.js for retrieve api and index.html tweaks

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,19 @@
+var APIKey = cf1929056b460b4693a80b30482c21ed
+var city;
+
+var fetchButton = document.getElementById('fetch-city')
 $(document).ready(function () {
     var weatherClock = function () {
         var presentClock = moment().format('[Today], dddd MMMM Do YYYY h:mm:ss a')
         $("#presentDay").text(presentClock)
     }
-
+function retrieveAPI(){
+    var requestSite = 'https://api.openweathermap.org/data/2.5/weather?q={Fremont}&appid={cf1929056b460b4693a80b30482c21ed}'
+    fetch(requestSite)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data)
 
 
 
@@ -14,3 +24,5 @@ $(document).ready(function () {
 
     setInterval(weatherClock, 1000)
 })
+
+fetchButton.addEventListener('click', retrieve)
