299d2828cc6f5a7a36d20daf781796d6e099f620
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-10-08 18:48:38 -0700

    five day forecast in console in script.js and index.html

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,43 @@
-var APIKey = cf1929056b460b4693a80b30482c21ed
-var city;
-
+// var APIKey = cf1929056b460b4693a80b30482c21ed
+// var city;
 var fetchButton = document.getElementById('fetch-city')
-$(document).ready(function () {
-    var weatherClock = function () {
-        var presentClock = moment().format('[Today], dddd MMMM Do YYYY h:mm:ss a')
-        $("#presentDay").text(presentClock)
-    }
-function retrieveAPI(){
-    var requestSite = 'https://api.openweathermap.org/data/2.5/weather?q={Fremont}&appid={cf1929056b460b4693a80b30482c21ed}'
+// $(document).ready(function () {
+//     var weatherClock = function () {
+//         var presentClock = moment().format('[Today], dddd MMMM Do YYYY h:mm:ss a')
+//         $("#presentDay").text(presentClock)
+//     }
+//     setInterval(weatherClock, 1000)
+// })
+// var city = document.getElementById("city").value
+// console.log(city)
+function retrieveAPI() {
+    var city = document.getElementById("city").value
+    console.log(city)
+    var requestSite = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=cf1929056b460b4693a80b30482c21ed'
+
     fetch(requestSite)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
 
+                console.log(data)
 
+                var lon = data.coord.lon
+                var lat = data.coord.lat
+                var requestNew = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=cf1929056b460b4693a80b30482c21ed'
 
+                fetch(requestNew)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        console.log(data)
+                    })
+            })
+}
 
+fetchButton.addEventListener('click', retrieveAPI)
 
-
-
-
-    setInterval(weatherClock, 1000)
-})
-
-fetchButton.addEventListener('click', retrieve)
+            // cf1929056b460b4693a80b30482c21ed
+            // f64f9e2d4fda40afd330c539b14a2d45
