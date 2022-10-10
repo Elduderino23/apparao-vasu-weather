550fc818fc04b66cfbad9f177b0095290494e028
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-10-09 19:46:19 -0700

    put icons in script.js

--- assets/script.js
+++ assets/script.js
@@ -19,7 +19,7 @@ console.log(city)
 function retrieveAPI() {
     var city = document.getElementById("city").value
     console.log(city)
-    var requestSite = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=cf1929056b460b4693a80b30482c21ed'
+    var requestSite = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=cf1929056b460b4693a80b30482c21ed&units=imperial'
 
     fetch(requestSite)
         .then(function (response) {
@@ -34,13 +34,14 @@ function retrieveAPI() {
 
             var lon = data.city.coord.lon
             var lat = data.city.coord.lat
-            var requestNew = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=cf1929056b460b4693a80b30482c21ed'
+            var requestNew = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=cf1929056b460b4693a80b30482c21ed&units=imperial'
 
             fetch(requestNew)
                 .then(function (response) {
                     return response.json();
                 })
                 .then(function (data) {
+                    renderOriginForecastCard(data)
                     console.log(data)
                     // weather.textContent = data.main.temp
                     // for (let i = 0; i < 5; i++) {
@@ -67,7 +68,7 @@ var cardOneHTML = `
 <div class="card">
   <div class="card-body">
   <p class="card-text">${dayOne}</p>
-    <h5 class="card-title">${todayWeather.main.temp}/h5>
+  <img class="card-title"src=${"http://openweathermap.org/img/wn/"+todayWeather.weather[0].icon+"@2x.png"}
     <p class="card-text">temp: ${todayWeather.main.temp}</p>
     <p class="card-text">wind speed: ${todayWeather.wind.speed}</p>
     <p class="card-text">wind gust: ${todayWeather.wind.gust}</p>
@@ -83,12 +84,12 @@ forecastListE0.appendChild(listItemOne)
 function renderForecastCard(weatherObject,i){
     console.log(weatherObject)
     var dayNext = moment().add(i+1,'days').format("MMM Do YY") 
-var forecastListEl = document.getElementById("weather")
+var forecastListEl = document.getElementById("js-forecast-list")
 var cardHTML = `
 <div class="card">
   <div class="card-body">
   <p class="card-text">${dayNext}</p>
-    <h5 class="card-title">${weatherObject.main.temp}/h5>
+    <img class="card-title"src=${"http://openweathermap.org/img/wn/"+weatherObject.weather[0].icon+"@2x.png"}>
     <p class="card-text">temp: ${weatherObject.main.temp}</p>
     <p class="card-text">wind speed: ${weatherObject.wind.speed}</p>
     <p class="card-text">wind gust: ${weatherObject.wind.gust}</p>
@@ -102,26 +103,28 @@ forecastListEl.appendChild(listItem)
 }
 // var day1 = moment().add(1,'day').format("MMM Do YY")
 // var day2 = moment().add(2,'day').format("MMM Do YY")
-function renderForecastCard(todayWeatherObject){
-    console.log(todayWeatherObject)
-var forecastListEl = document.getElementById("js-forecast-list")
-var cardHTML = `
-<div class="card">
-  <div class="card-body">
-    <h5 class="card-title">temp: ${todayWeatherObject.main.temp}>
-    <p class="card-text">temp: ${todayWeatherObject.main.temp}</p>
-    <p class="card-text">wind speed: ${todayWeatherObject.wind.speed}</p>
-    <p class="card-text">wind gust: ${todayWeatherObject.wind.gust}</p>
-    <p class="card-text">humidity: ${todayWeatherObject.main.humidity}</p>
-  </div>
-</div>
-`
-var listItem = document.createElement("li")
-listItem.innerHTML=cardHTML
-forecastListEl.appendChild(listItem)
-}
+// function renderForecastCard(todayWeatherObject){
+//     console.log(todayWeatherObject)
+// var forecastListEl = document.getElementById("fetch-weather")
+// var cardHTML = `
+// <div class="card">
+//   <div class="card-body">
+//     <h5 class="card-title">temp: ${todayWeatherObject.main.temp}>
+//     <p class="card-text">temp: ${todayWeatherObject.main.temp}</p>
+//     <p class="card-text">wind speed: ${todayWeatherObject.wind.speed}</p>
+//     <p class="card-text">wind gust: ${todayWeatherObject.wind.gust}</p>
+//     <p class="card-text">humidity: ${todayWeatherObject.main.humidity}</p>
+//   </div>
+// </div>
+// `
+// var listItem = document.createElement("li")
+// listItem.innerHTML=cardHTML
+// forecastListEl.appendChild(listItem)
+// }
 
 fetchButton.addEventListener('click', retrieveAPI)
 
 // cf1929056b460b4693a80b30482c21ed
 // f64f9e2d4fda40afd330c539b14a2d45
+// ${src="http://openweathermap.org/img/wn/"+weatherObject.list[0].weather[0].icon+"@2x.png"}
+// http://openweathermap.org/img/wn/10d@2x.png
